48af926a927aa21884d9a5dc0f76c3f4f93fd03a
Albert Golubev <agolubev@Alberts-MBP.fios-router.home>
2022-03-07 23:49:42 -0500

    part 3 is done

--- pom/pageUsage.spec.js
+++ pom/pageUsage.spec.js
@@ -2,8 +2,25 @@ import loginPage from '../loginPage'
 
 describe('POM Implementation',()=> {
 
-	beforeEach() => {
+	beforeEach(() => {
 		cy.visit('https://www.saucedemo.com')
 
 	});
+
+	it('should login to inventory page', () =>
+	{
+
+		loginPage.enterUsername('standard_user')
+		loginPage.enterPassword('secret_sauce')
+		loginPage.loginAction()
+
+	});
+
+
+
+	addOnlyTshirts(() => {
+		cy.get('*[id^="t-shirt"]')
+
+	});
+
 });
